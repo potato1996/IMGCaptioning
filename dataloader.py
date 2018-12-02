962e23a7b0e445d63761c0a9119e1c9a34e29e48
xchen <xc1113@nyu.edu>
2018-12-02 14:39:27 -0500

    change vocab to self.vocab $ formatting

--- dataloader.py
+++ dataloader.py
@@ -12,14 +12,18 @@ import nltk
     What's changed:
         COCO dataset provide 5 different captions for the same image. We only need 1 for training
 """
+
+
 class CocoCaptionsTrain(data.Dataset):
     """`MS Coco Captions <http://mscoco.org/dataset/#captions-challenge2015>`_ Dataset.
 
     """
+
     def __init__(self, vocab, root, annFile, transform=None):
         from pycocotools.coco import COCO
         self.root = os.path.expanduser(root)
         self.coco = COCO(annFile)
+        self.vocab = vocab
         self.ids = list(self.coco.imgs.keys())
         self.transform = transform
 
@@ -47,14 +51,15 @@ class CocoCaptionsTrain(data.Dataset):
         target = target[0]
         tokens = nltk.tokenize.word_tokenize(str(target).lower)
         target = []
-        target.append(vocab.word2vec("<start>"))
-        target.extend([vocab.word2vec(token) for token in tokens])
-        target.append(vocab.word2vec("<end>"))
+        target.append(self.vocab.word2vec("<start>"))
+        target.extend([self.vocab.word2vec(token) for token in tokens])
+        target.append(self.vocab.word2vec("<end>"))
         target = torch.Tensor(target)
         return img, target
 
     def __len__(self):
         return len(self.ids)
+
 
 def train_collate_fn(data):
     """ Costomized mini-batch creation function 
@@ -74,20 +79,23 @@ def train_collate_fn(data):
     images = torch.stack(images, 0)
 
     # create mini-batch target
-    lengths  = [len(caption) for caption in captions]
+    lengths = [len(caption) for caption in captions]
     minibatch_maxlen = max(lengths)
     targets = torch.zeros(batch_size, minibatch_maxlen).long()
-    
+
     # copy the valid parts into targets
     for i, cap in enumerate(captions):
         targets[i, :lengths[i]] = cap[:lengths[i]]
-    
+
     return images, targets, lengths
-    
+
+
 def get_train_loader(vocab, root, annFile, transform, batch_size, shuffle=True, num_workers=1):
     train_set = CocoCaptionsTrain(vocab, root, annFile, transform)
-    train_set_loader = torch.utils.data.DataLoader(dataset = train_set, batch_size = batch_size, shuffle=shuffle, num_workers=num_workers, collate_fn=train_collate_fn)
+    train_set_loader = torch.utils.data.DataLoader(dataset=train_set, batch_size=batch_size, shuffle=shuffle,
+                                                   num_workers=num_workers, collate_fn=train_collate_fn)
     return train_set_loader
+
 
 def get_val_loader(root, annFile, transform, shuffle=False, num_workers=1):
     val_set = datasets.CocoCaptions(root, annFile, transform=transform, target_transform=None)
